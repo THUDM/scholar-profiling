cf1f41d25d66c43c82826e3d8ac94a83cdb3891c
ylinQAQ <105409465+ylinQAQ@users.noreply.github.com>
2023-08-01 11:15:23 +0800

    Update model.py

--- bio_models/CNN_base/models/model.py
+++ bio_models/CNN_base/models/model.py
@@ -1,4 +1,6 @@
 from torch import nn
+from fastNLP import seq_len_to_mask
+from torch_scatter import scatter_max
 import torch
 import torch.nn.functional as F
 from .cnn import MaskCNN
@@ -48,23 +50,24 @@ class CNNNer(nn.Module):
         self.down_fc = nn.Linear(cnn_dim, num_ner_tag)
         self.logit_drop = logit_drop
 
-    # matrix -> (b, s, s, l)
-    def forward(self, input_ids, attention_mask, token_type_ids, matrix=None):
-        # bert
-        outputs = self.pretrain_model(input_ids, attention_mask, token_type_ids, return_dict=True)
-        state = outputs['last_hidden_state']
-        # 构建双仿射矩阵
+    def forward(self, input_ids, bpe_len, indexes, matrix):
+        attention_mask = seq_len_to_mask(bpe_len)  # bsz x length x length
+        outputs = self.pretrain_model(input_ids, attention_mask=attention_mask, return_dict=True)
+        last_hidden_states = outputs['last_hidden_state']
+        state = scatter_max(last_hidden_states, index=indexes, dim=1)[0][:, 1:]  # bsz x word_len x hidden_size
+        lengths, _ = indexes.max(dim=-1)
+
         head_state = self.head_mlp(state)
         tail_state = self.tail_mlp(state)
-        if hasattr(self, 'U'): # (bs, r, n, n)
-            scores1 = torch.einsum('bxi, oij, byj -> boxy', head_state, self.U, tail_state) # 不使用多头
+        if hasattr(self, 'U'):
+            scores1 = torch.einsum('bxi, oij, byj -> boxy', head_state, self.U, tail_state)
         else:
             scores1 = self.multi_head_biaffine(head_state, tail_state)
         head_state = torch.cat([head_state, torch.ones_like(head_state[..., :1])], dim=-1)
         tail_state = torch.cat([tail_state, torch.ones_like(tail_state[..., :1])], dim=-1)
         affined_cat = torch.cat([self.dropout(head_state).unsqueeze(2).expand(-1, -1, tail_state.size(1), -1),
                                  self.dropout(tail_state).unsqueeze(1).expand(-1, head_state.size(1), -1, -1)], dim=-1)
-        # 位置信息嵌入
+
         if hasattr(self, 'size_embedding'):
             size_embedded = self.size_embedding(self.span_size_ids[:state.size(1), :state.size(1)])
             affined_cat = torch.cat([affined_cat,
@@ -74,16 +77,19 @@ class CNNNer(nn.Module):
         scores = scores2 + scores1   # bsz x dim x L x L
 
         if hasattr(self, 'cnn'):
-            mask = attention_mask
-            mask = mask[:, None] * mask.unsqueeze(-1) # (b, 1, s) * (b, s,1)
+            mask = seq_len_to_mask(lengths)  # bsz x length x length
+            mask = mask[:, None] * mask.unsqueeze(-1)
             pad_mask = mask[:, None].eq(0)
             u_scores = scores.masked_fill(pad_mask, 0)
             if self.logit_drop != 0:
                 u_scores = F.dropout(u_scores, p=self.logit_drop, training=self.training)
+            # bsz, num_label, max_len, max_len = u_scores.size()
             u_scores = self.cnn(u_scores, pad_mask)
             scores = u_scores + scores
 
         scores = self.down_fc(scores.permute(0, 2, 3, 1))
+
+        assert scores.size(-1) == matrix.size(-1)
 
         if self.training:
             flat_scores = scores.reshape(-1)
@@ -91,6 +97,7 @@ class CNNNer(nn.Module):
             mask = flat_matrix.ne(-100).float().view(input_ids.size(0), -1)
             flat_loss = F.binary_cross_entropy_with_logits(flat_scores, flat_matrix.float(), reduction='none')
             loss = ((flat_loss.view(input_ids.size(0), -1)*mask).sum(dim=-1)).mean()
+            
             return {'loss': loss}
 
         return {'scores': scores}
